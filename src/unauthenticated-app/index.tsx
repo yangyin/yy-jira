ae6722d82698ae3882cb5f7ac4b6db1892d177a2
yy\杨银 <182316962@qq.com>
2022-09-27 22:08:25 +0800

    feat: css in js

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,17 +1,35 @@
 import { useState } from 'react'
-import { Card, Button } from 'antd'
+import { Card, Button, Divider } from 'antd'
 import { LoginScreen } from './login'
 import { RegisterScreen } from './register'
+import styled from '@emotion/styled'
 
 export const UnauthenticatedApp = () => {
     const [isRegister, setIsRegister ] = useState(false)
 
-    return <div style={{display: 'flex', justifyContent: 'center'}}>
-        <Card>
+    return <Container>
+        
+        <ShadowCard>
         {
             isRegister ? <RegisterScreen /> : <LoginScreen />
         }
-        <Button onClick={() => setIsRegister(!isRegister)}>切换到{isRegister ? '登录' : '注册'}</Button>
-        </Card>
-    </div>
+        <Divider />
+        <a onClick={() => setIsRegister(!isRegister)}>{isRegister ? '已经有账号了？直接登录' : '没有账号？注册新账号'}</a>
+        </ShadowCard>
+    </Container>
 }
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+min-height: 100vh;
+`
+const ShadowCard = styled(Card)`
+width: 40rem;
+min-height: 46rem;
+padding: 3.2rem 4rem;
+border-radius: .3rem;
+box-shadow: rgba(0,0,0,0.1) 0 0 10px;
+text-align: center;
+`
